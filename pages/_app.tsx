25338ab3d256ec2640ecc4c4d53eeaa1943c8f2e
Reno Ardiansyah <mail.renoardiansyah@gmail.com>
2024-12-24 21:04:58 +0700

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+/**
+ * Home Page of the Application
+ * @return {JSX.Element}
+ */
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
